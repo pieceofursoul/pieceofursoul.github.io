c5b5a4b8ac389322cf0a828c727dfaed8161b80a
pieceofursoul <83923465+pieceofursoul@users.noreply.github.com>
2021-05-13 15:27:13 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -50,34 +50,9 @@ $(function() {
 
 });
 
-/* Collapse откр/закр*/
-$("[data-collapse]").on("click", function(event){
-   event.preventDefault();
-
-   var $this = $(this),
-       blockId = $this.data('collapse');
-
-       $this.toggleClass("active");
-       $(blockId).slideToggle();
-});
 
 
-/* Contacts */
 
-$(window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-});
 
 
 
